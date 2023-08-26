804e03acb6edd42f665c278331f9d4c7b9d232bb
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-08-26 12:30:26 +0300

    fix/I18NEXT-NEXT: changed settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  output: 'export',
   skipTrailingSlashRedirect: true,
   distDir: 'dist',
   reactStrictMode: true,
