3381f99eb351dc8dfbbf26179cf1dae36c4dc243
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-08-26 14:10:32 +0300

    fix/I18NEXT-NEXT: changed settings

---
+++ dist/server/_rsc_messages_pt_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_messages_pt_json";
+exports.ids = ["_rsc_messages_pt_json"];
+exports.modules = {
+
+/***/ "(rsc)/./messages/pt.json":
+/*!**************************!*\
+  !*** ./messages/pt.json ***!
+  \**************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"navbar":{"home":"dom","about":"około","contact":"kontakt"},"homepage":{"welcome-message":"Witamy w stylu JavaScript","topic":"Temat do omówienia","client-side-component":"Tylko 328 subskrybentów","tab-title-home":"strona główna"},"about":{"title":"To jest moja strona O mnie","tab-title-about":"about"},"contact":{"title":"To jest moja strona KONTAKT","tab-title-contact":"about"},"footer":{"description":"To jest moja stopka"}}');
+
+/***/ })
+
+};
+;
